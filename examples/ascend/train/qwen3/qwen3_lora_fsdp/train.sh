389839d27c97337a842d632ac59e82d3e3890809
addsubmuldiv <zyh13227@163.com>
2026-03-12 16:58:12 +0800

    update npu fsdp example

--- examples/ascend/train/qwen3/qwen3_lora_fsdp/train.sh
+++ examples/ascend/train/qwen3/qwen3_lora_fsdp/train.sh
@@ -1,8 +1,11 @@
 # hardware: Atlas 900 A2
+# For NPU, in Transformers versions 5.0 and above, it is recommended to disable
+# cpu_ram_efficient_loading in fsdp.json to avoid timeout issues at the first 
+# synchronization point caused by inter-card desynchronization when loading the model.
 export TASK_QUEUE_ENABLE=2
 export CPU_AFFINITY_CONF=2
 ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
-accelerate launch --config_file "./examples/ascend/train/qwen3_lora_fsdp/fsdp.json" \
+accelerate launch --config_file "./examples/ascend/train/qwen3/qwen3_lora_fsdp/fsdp.json" \
     swift/cli/sft.py \
     --model 'Qwen/Qwen3-32B' \
     --tuner_type lora \
